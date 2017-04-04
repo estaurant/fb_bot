04c7ffc56586867325a8fe59c0594d3a429bd1ab
rinnirt@hotmail.com <rin@Rins-MacBook-Pro.local>
2017-04-04 14:04:20 +0700

    fix bug context is undefined

--- index.js
+++ index.js
@@ -109,11 +109,10 @@ app.post('/webhook', (req, res) => {
       );
     } else if (msg) {
       // We received a text message
-      onMessage(msg, context);
       // Let's forward the message to the Wit.ai Bot Engine
       // This will run all actions until our bot has nothing left to do
       var context = sessions[sessionId].context;
-      
+      onMessage(msg, context);
 
       
     }
