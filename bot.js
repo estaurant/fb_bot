d619a67be4f9a864992ddd594eb96536f0e17a21
rinnirt@hotmail.com <rin@Rins-MacBook-Pro.local>
2017-04-09 07:45:23 +0700

    Revert "test template response"
    This reverts commit 6c0fb6b566a34cb8b779064d0c04a2cc1c9b8592.

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ const findIntent = (msg) => {
   var greetingList = ["สวัสดี", "hello", "hi", "หวัดดี" ];
   var intent;
   for (var i = 0; i < greetingList.length; i++) { 
-    if (msg.toLowerCase().startsWith(greetingList[i])) {
+    if (msg.startsWith(greetingList[i])) {
       return "greeting";
     }
   }
@@ -123,11 +123,6 @@ const findIntent = (msg) => {
 const estaurantMessage = (msg, context) => {
   var intent = findIntent(msg);
   var keyword;
-
-const estaurantMessage = (msg, context) => {
-  if (msg === 'test') {
-    fbSend(buildGenericTemplate({}) ,context);
-  }
 
   if (intent === 'food') {
     fbTextSend("แปปนึงนะ", context);
@@ -206,41 +201,4 @@ if (require.main === module) {
   // interactive(client);
   rlInteractive(client);
 
-}
-
-const buildGenericTemplate =(result) => {
-  return {
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"generic",
-            "elements":[
-              {
-                "title":"Welcome to Peter\'s Hats",
-                "image_url":"https://petersfancybrownhats.com/company_image.png",
-                "subtitle":"We\'ve got the right hat for everyone.",
-                "default_action": {
-                  "type": "web_url",
-                  "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                  "messenger_extensions": true,
-                  "webview_height_ratio": "tall",
-                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                },
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":"https://petersfancybrownhats.com",
-                    "title":"View Website"
-                  },{
-                    "type":"postback",
-                    "title":"Start Chatting",
-                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                  }              
-                ]      
-              }
-            ]
-          }
-        }
-      };
-    }
 }
