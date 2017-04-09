8f617283719839a9e3b842e5034da116fb493323
rinnirt@hotmail.com <rin@Rins-MacBook-Pro.local>
2017-04-09 10:27:25 +0700

    comment old callSentenceAi

--- api.js
+++ api.js
@@ -93,79 +93,79 @@ const isMatchIntent = (intentList, inverseIntentList, keyword) => {
     return matchIntent && !matchInverseIntent
 }
 
-const callSentenceAi = (sentence) => {
-    var options = {
-        uri: Config.SENTENCE_AI_URL,
-        method: 'POST',
-        headers: {
-            "content-type": "application/json",
-        },
-        json: {
-            "sentence": sentence
-        }
-    };
+// const callSentenceAi = (sentence) => {
+//     var options = {
+//         uri: Config.SENTENCE_AI_URL,
+//         method: 'POST',
+//         headers: {
+//             "content-type": "application/json",
+//         },
+//         json: {
+//             "sentence": sentence
+//         }
+//     };
 
-    var callback = function (error, response, body) {
-        var intentAndWord = {};
-        if (!error && response.statusCode == 200) {
-            intentAndWord = {
-                "intent" : response.body.intent,
-                "word"   : response.body.word
-            };
-        } 
-        return intentAndWord;
-    }
+//     var callback = function (error, response, body) {
+//         var intentAndWord = {};
+//         if (!error && response.statusCode == 200) {
+//             intentAndWord = {
+//                 "intent" : response.body.intent,
+//                 "word"   : response.body.word
+//             };
+//         } 
+//         return intentAndWord;
+//     }
     
-    return request(options, callback);
+//     return request(options, callback);
 
-    var onMessage = function () {
-        //TODO call sentence_ai
-      var sentence = "xxx";
-      var options = {
-          uri: Config.SENTENCE_AI_URL,
-          method: 'POST',
-          headers: {
-              "content-type": "application/json",
-          },
-          json: {
-              "sentence": sentence
-          }
-      };
+//     var onMessage = function () {
+//         //TODO call sentence_ai
+//       var sentence = "xxx";
+//       var options = {
+//           uri: Config.SENTENCE_AI_URL,
+//           method: 'POST',
+//           headers: {
+//               "content-type": "application/json",
+//           },
+//           json: {
+//               "sentence": sentence
+//           }
+//       };
       
-      request(options, function (error, response, body) {
-        var intentAndWord = {};
-        if (!error && response.statusCode == 200) {
-            intentAndWord = {
-                "intent" : response.body.intent,
-                "word"   : response.body.word
-            };
-        } 
+//       request(options, function (error, response, body) {
+//         var intentAndWord = {};
+//         if (!error && response.statusCode == 200) {
+//             intentAndWord = {
+//                 "intent" : response.body.intent,
+//                 "word"   : response.body.word
+//             };
+//         } 
 
-        var queryString = { 
-          keyword: response.body.word,
-          distance: '1km',
-          price: 100
-        };
+//         var queryString = { 
+//           keyword: response.body.word,
+//           distance: '1km',
+//           price: 100
+//         };
 
-        request({url:Config.RESTAURANT_API_URL, qs:queryString}, function(err, response, body) {
-            if(err) { console.log(err); return; }
-            var restaurants = JSON.parse(response.body);
-            var result = "ไก่แป้ง";
-            if (restaurants[0]) {
-              result = restaurants[0]._source.name;
-            }
-            FB.fbMessage(
-              sender,
-              {text:'ตะมุตะมิ ลองดูไหมเธอ ร้าน' + result}
-            );
-            res.sendStatus(200);
-          }
-        );
+//         request({url:Config.RESTAURANT_API_URL, qs:queryString}, function(err, response, body) {
+//             if(err) { console.log(err); return; }
+//             var restaurants = JSON.parse(response.body);
+//             var result = "ไก่แป้ง";
+//             if (restaurants[0]) {
+//               result = restaurants[0]._source.name;
+//             }
+//             FB.fbMessage(
+//               sender,
+//               {text:'ตะมุตะมิ ลองดูไหมเธอ ร้าน' + result}
+//             );
+//             res.sendStatus(200);
+//           }
+//         );
 
         
-      });
-    }
-}
+//       });
+//     }
+// }
 
 module.exports = {
   callRestaurantApi: callRestaurantApi,
